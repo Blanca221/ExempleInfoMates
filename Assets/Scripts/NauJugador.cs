4f36a700d34fd4d1977ee5153b4ae411c2060ba5
Blanca Miranda <blancamiranda@ceroca.cat>
2024-09-26 15:22:04 +0200

    Proyecto Iniciado

---
+++ Assets/Scripts/NauJugador.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NauJugador : MonoBehaviour
+{
+    private float _vel;
+
+    private Vector2 minPantalla, maxPantalla;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _vel = 8f;
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
+        float direccioIndicadaY = Input.GetAxisRaw("Vertical");
+        //Debug.Log("X: " + direccioIndicadaX + " - Y: " + direccioIndicadaY);
+        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY).normalized;
+
+        Vector2 novaPos = transform.position;// transform.position: pos actual de la nau.
+        novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
+        //Debug.Log(Time.deltaTime);
+
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
+        
+        transform.position = novaPos;
+    }
+}
